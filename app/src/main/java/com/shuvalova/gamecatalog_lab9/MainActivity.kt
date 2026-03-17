a8faa646cf4113b30815c38f433adfc4d9094581
Forinarr <shuvalova0723@list.ru>
2026-03-17 22:09:59 +0300

    add screenshots and prototype

--- app/src/main/java/com/shuvalova/gamecatalog_lab9/MainActivity.kt
+++ app/src/main/java/com/shuvalova/gamecatalog_lab9/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.shuvalova.gamecatalog_lab9.ui.theme.Gamecatalog_lab9Theme
+import gamesList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +30,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Gamecatalog_lab9Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
                 }
             }
         }
@@ -43,5 +54,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Gamecatalog_lab9Theme {
         Greeting("Android")
+    }
+}
+@Composable
+fun MainScreen() {
+    var currentIndex by remember { mutableStateOf(0) }
+    var isFavorite by remember { mutableStateOf(false) }
+
+    val currentGame = gamesList[currentIndex]
+
+    val onNext = {
+        currentIndex = (currentIndex + 1) % gamesList.size
+        isFavorite = false
+    }
+    val onPrev = {
+        currentIndex = (currentIndex - 1 + gamesList.size) % gamesList.size
+        isFavorite = false
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        GameImageSection(imageResId = currentGame.imageResId)
+        GameInfoSection(game = currentGame)
+
+        Spacer(modifier = Modifier.weight(1f))
+        ControlPanel(
+            onPrevClick = onPrev,
+            onNextClick = onNext,
+            isFavorite = isFavorite,
+            onFavoriteChange = { isFavorite = it }
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Gamecatalog_lab9Theme {
+        MainScreen()
     }
 }
